a19bfbc8959532d7155a44f1eb0e0c06803b3770
AndreiVelmezev <velmezev@gmail.com>
2022-06-20 01:24:17 +0300

    ДЗ готово

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ import java.util.logging.Level;
 
 public class Main {
     public static void main(String[] args) {
+        Author author1 = new Author("Lev","Tolstoy");
+        Book book1=new Book("Voyna i mir", author1,1865);
+        System.out.println("Name book "+ book1.getBookName()+ " Name autor "+book1.getAuthor().getName()
+                + " "+ book1.getAuthor().getSurname()+" god publikacii "+ book1.getPublishingYear());
+
+        book1.setPublishingYear(1869);
+
+        System.out.println("Name book "+ book1.getBookName()+ " Name autor "+book1.getAuthor().getName()
+                + " "+ book1.getAuthor().getSurname()+" god publikacii "+ book1.getPublishingYear());
+
 
     }
 }
